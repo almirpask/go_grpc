2fb9269915380564517a68d6307a37a20206ca34
almirpask <almirpask@hotmail.com>
2024-02-11 16:47:09 -0300

    add sqlite adpater

--- cmd/grpcServer/main.go
+++ cmd/grpcServer/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/almirpask/go_grpc/internal/database"
 	"github.com/almirpask/go_grpc/internal/pb"
 	"github.com/almirpask/go_grpc/internal/services"
+	_ "github.com/mattn/go-sqlite3"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
